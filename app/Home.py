c9a3a872b7bfd1bec77b5fbcf0b23de95545d366
darylkdps <darylkdps-github@yahoo.com>
2023-02-08 10:10:03 +0800

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from pathlib import Path
 
-st.title('Research')
 st.set_page_config(
     page_title='Research',
     page_icon='random',
     layout='wide',
     initial_sidebar_state='auto'
     )
+
+st.title('Research')
 
 # if 'D:' in str(Path.cwd()) or 'C:' in str(Path.cwd()):
 #     st.markdown('_No token_')
