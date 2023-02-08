a909591b801213636be8fbcbd3bf20429b5215fd
darylkdps <92910794+darylkdps@users.noreply.github.com>
2023-02-08 10:05:13 +0800

    Add files via upload

---
+++ app/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from pathlib import Path
+
+st.title('Research')
+st.set_page_config(
+    page_title='Research',
+    page_icon='random',
+    layout='wide',
+    initial_sidebar_state='auto'
+    )
+
+# if 'D:' in str(Path.cwd()) or 'C:' in str(Path.cwd()):
+#     st.markdown('_No token_')
+# else:
+#     st.markdown('_' + st.secrets['test_token'] + '_')
+
+
