f37d56db7efa514572f80a1c0dce5de920656b17
darylkdps <darylkdps-github@yahoo.com>
2023-02-08 10:34:37 +0800

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -43,6 +43,7 @@ fig = px.bar(
     df,
     x='Cosine Similarity',
     y='Content Word',
+    custom_data=[title_word_cleaned],
     range_x=[0,1],
     text_auto='.3f',
     text='Cosine Similarity',
@@ -54,7 +55,7 @@ fig = px.bar(
 
 fig.update_traces(
     marker_color='cyan',
-    hovertemplate=('%{title_word_cleaned} - %{y} Cosine Similarity: %{x}'),
+    hovertemplate=('%{custom_data[0]} - %{y}: %{x}'),
     textposition='outside',
     texttemplate='%{x:.3f}',
     )
