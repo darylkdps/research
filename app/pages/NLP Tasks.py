e202b8ba114254d34ca94843653f284d39b4527d
darylkdps <darylkdps-github@yahoo.com>
2023-03-31 00:41:05 +0800

    Update NLP Tasks.py

--- app/pages/NLP Tasks.py
+++ app/pages/NLP Tasks.py
@@ -13,17 +13,42 @@ st.set_page_config(
     initial_sidebar_state='collapsed'  # auto, expanded, collapsed
     )
 
+if 'C:\\' in str(Path.cwd()) or 'D:\\' in str(Path.cwd()):
+    data_path = Path.cwd() / 'pages'
+else:
+    data_path = Path.cwd() / 'app' / 'pages'
+
 @st.cache_resource
-def load_model():
+def load_nlp_model():
     spacy.require_cpu()
     return spacy.load('en_core_web_lg')
-nlp = load_model()
+nlp = load_nlp_model()
 
 caption_placeholder = st.empty()
 st.title('NLP Tasks')
 metadata1 = {key:value for key, value in nlp.meta.items() if key in {'lang', 'name', 'version', 'vectors'}}
 metadata2 = f"*spaCy={spacy.__version__}, {metadata1['lang']}_{metadata1['name']}={metadata1['version']} ({metadata1['vectors']['vectors']} vectors with {metadata1['vectors']['width']} dimensions)*"
 # caption_placeholder.caption(metadata2)
+
+@st.cache_resource
+def load_df():
+    df_file = data_path / 'df_fake_true_clean.pkl'
+    df = pd.read_pickle(df_file)
+    index = [i for i in range(0, 50)] + [i for i in range(len(df) - 50, len(df))]
+    return df.iloc[index]
+df = load_df()
+
+@st.cache_resource
+def load_scaler():
+    scaler_file = data_path / 'minmaxscaler_model.sav'
+    return pickle.load(open(scaler_file, 'rb'))
+loaded_minmaxscaler_model = load_scaler()
+
+@st.cache_resource
+def load_classifier():
+    classifier_file = data_path / 'kneighborsclassifier_model.sav'
+    return pickle.load(open(classifier_file, 'rb'))
+loaded_kneighborsclassifier_model = load_classifier()
 
 # Tokenisation
 st.header('Tokenisation')
@@ -231,52 +256,46 @@ if st.button('Vectorise', key='vectorise_button1'):
 
 st.subheader('*Machine learning: text classification*')
 
-st.markdown('''Reference true news:
 
-Turkish authorities have issued detention warrants for 216 people, including former finance ministry personnel, suspected 
-of having links to last year's failed coup attempt, the state-run Anadolu news agency said on Wednesday. It said 17 former 
-finance ministry personnel had been detained so far and another 65 were sought over alleged links to Gulen's network, Anadolu 
-said. Separately, authorities carried out operations across 40 provinces targeting private imams believed to be recruiting members 
-to the network of U.S.-based cleric Fethullah Gulen from Turkey's armed forces. Ankara blames Gulen for orchestrating the 
-July 15 coup attempt last year and has repeatedly demanded the United States extradite him, so far in vain. Gulen denies 
-involvement. In the aftermath of the coup, more than 50,000 people have been jailed pending trial and some 150,000 have 
-been sacked or suspended from their jobs in the military, public and private sector. The extent of the purges has unnerved 
-rights groups and Turkey's Western allies, who fear President Tayyip Erdogan is using the abortive putsch as a pretext 
-to stifle dissent. The government, however, says the measures are necessary due to the gravity of the threats it is facing 
-following the military coup attempt, in which 240 people were killed.
-''')
 
-st.markdown('''Reference fake news:
 
-Hollywood stars took to social media Wednesday to express outrage over President Donald Trump's announcement that transgender 
-people will not be allowed to serve in the U.S. military, reversing former President Obama's decision last year allowing 
-them to do so. After consultation with my Generals and military experts, please be advised that the United States Government 
-will not accept or allow Transgender individuals to serve in any capacity in the U.S. Military, President Trump tweeted 
-Wednesday morning. Our military must be focused on decisive and overwhelming victory and cannot be burdened with the tremendous 
-medical cost and disruption that transgender in the military would entail. Thank you. The news spurred some stars to viciously 
-attack the president, with some calling Trump a cruel bigot. Star Trek actor and gay activist George Takei sent what appeared 
-to be a threat to Trump, warning that he  just pissed off the wrong community  and said the president  will regret this 
-action. Donald: With your ban on trans people from the military, you are on notice that you just pissed off the wrong community. 
-You will regret it. Takei also tweeted: History shall record that you are not only the stupidest, most incompetent president 
-ever, but also the cruelest and pettiest.
-''')
+st.dataframe(df, width=None, height=None, use_container_width=True)
 
-if 'D:' in str(Path.cwd()):
-    data_path = Path.cwd() / 'pages'
-else:
-    data_path = Path.cwd() / 'app' / 'pages'
+# st.markdown('''Reference true news:
 
-scaler_file = data_path / 'minmaxscaler_model.sav'
-classifier_file = data_path / 'kneighborsclassifier_model.sav'
+# Turkish authorities have issued detention warrants for 216 people, including former finance ministry personnel, suspected 
+# of having links to last year's failed coup attempt, the state-run Anadolu news agency said on Wednesday. It said 17 former 
+# finance ministry personnel had been detained so far and another 65 were sought over alleged links to Gulen's network, Anadolu 
+# said. Separately, authorities carried out operations across 40 provinces targeting private imams believed to be recruiting members 
+# to the network of U.S.-based cleric Fethullah Gulen from Turkey's armed forces. Ankara blames Gulen for orchestrating the 
+# July 15 coup attempt last year and has repeatedly demanded the United States extradite him, so far in vain. Gulen denies 
+# involvement. In the aftermath of the coup, more than 50,000 people have been jailed pending trial and some 150,000 have 
+# been sacked or suspended from their jobs in the military, public and private sector. The extent of the purges has unnerved 
+# rights groups and Turkey's Western allies, who fear President Tayyip Erdogan is using the abortive putsch as a pretext 
+# to stifle dissent. The government, however, says the measures are necessary due to the gravity of the threats it is facing 
+# following the military coup attempt, in which 240 people were killed.
+# ''')
 
-loaded_minmaxscaler_model = pickle.load(open(scaler_file, 'rb'))
-loaded_kneighborsclassifier_model = pickle.load(open(classifier_file, 'rb'))
+# st.markdown('''Reference fake news:
+
+# Hollywood stars took to social media Wednesday to express outrage over President Donald Trump's announcement that transgender 
+# people will not be allowed to serve in the U.S. military, reversing former President Obama's decision last year allowing 
+# them to do so. After consultation with my Generals and military experts, please be advised that the United States Government 
+# will not accept or allow Transgender individuals to serve in any capacity in the U.S. Military, President Trump tweeted 
+# Wednesday morning. Our military must be focused on decisive and overwhelming victory and cannot be burdened with the tremendous 
+# medical cost and disruption that transgender in the military would entail. Thank you. The news spurred some stars to viciously 
+# attack the president, with some calling Trump a cruel bigot. Star Trek actor and gay activist George Takei sent what appeared 
+# to be a threat to Trump, warning that he  just pissed off the wrong community  and said the president  will regret this 
+# action. Donald: With your ban on trans people from the military, you are on notice that you just pissed off the wrong community. 
+# You will regret it. Takei also tweeted: History shall record that you are not only the stupidest, most incompetent president 
+# ever, but also the cruelest and pettiest.
+# ''')
 
 true_news_input1 = st.text_area(
     label='Input some news:',
     value='''An old review of an academic monograph on agrarian revolutionaries in 1930s China is hardly a political third rail in Beijing today, even by the increasingly sensitive standards of the ruling Communist Party. That such a piece appeared on a list of some 300 scholarly works that Cambridge University Press (CUP) said last week the Chinese government had asked it to block from its website offers clues about the inner workings of China s vast and secretive censorship apparatus, say experts. President Xi Jinping has stepped up censorship and tightened controls on the internet and various aspects of civil society, as well as reasserting Communist Party authority over academia and other institutions, since coming to power in 2012.''',
     height=120,
-    max_chars=1000,
+    max_chars=3000,
     key='true_news_input1',
     )
 if st.button('Classify news', key='classify_true_news_button1'):
@@ -291,7 +310,7 @@ fake_news_input1 = st.text_area(
     label='Input some news:',
     value='''Our reality is carefully constructed by powerful corporate, political and special interest sources in order to covertly sway public opinion. Blatant lies are often televised regarding terrorism, food, war, health, etc. They are fashioned to sway public opinion and condition viewers to accept what have become destructive societal norms. The practice of manipulating and controlling public opinion with distorted media messages has become so common that there is a whole industry formed around this.''',
     height=120,
-    max_chars=1000,
+    max_chars=3000,
     key='fake_news_input1',
     )
 if st.button('Classify news', key='classify_fake_news_button1'):
