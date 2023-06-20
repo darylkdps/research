334dbac58c7a468c23acd90933111229041b5a75
darylkdps <darylkdps-github@yahoo.com>
2023-06-20 18:03:49 +0800

    Update Ideation Scoring 2.py

--- app/pages/Ideation Scoring 2.py
+++ app/pages/Ideation Scoring 2.py
@@ -271,7 +271,7 @@ sl_de = st.data_editor(
     kwargs=None
 )
 
-col1, col2, _ = st.columns([0.1, 0.1, 1.3], gap='small')
+col1, col2, _ = st.columns([0.15, 0.15, 1.3], gap='small')
 
 with col1:
     clear_data_editor_button = st.button(
