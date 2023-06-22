60236adefc9d67b98092cb54778294214e96e4ed
darylkdps <darylkdps-github@yahoo.com>
2023-06-22 14:23:18 +0800

    set sidebar to collapse

--- app/pages/Ideation Scoring 1.py
+++ app/pages/Ideation Scoring 1.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title='Ideation Scoring 1',
     page_icon='📏',
     layout='wide',
-    initial_sidebar_state='auto'
+    initial_sidebar_state='collapsed'
     )
 
 caption_placeholder = st.empty()
--- app/pages/Ideation Scoring 2.py
+++ app/pages/Ideation Scoring 2.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title='Ideation Scoring 2',
     page_icon='📏',
     layout='wide',
-    initial_sidebar_state='auto'
+    initial_sidebar_state='collapsed'
     )
 
 caption_placeholder = st.empty()
