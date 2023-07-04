9611311d958fa30f403524f22ad11ee3ea40538e
darylkdps <darylkdps-github@yahoo.com>
2023-07-04 12:57:33 +0800

    Create Chatbot.py

---
+++ app/pages/Chatbot.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Chatbot',
+    page_icon='📏',
+    layout='wide',
+    initial_sidebar_state='auto'  # auto, expanded, collapsed
+    )
+
+if 'C:\\' in str(Path.cwd()) or 'D:\\' in str(Path.cwd()):
+    data_path = Path.cwd() / 'pages'
+else:
+    data_path = Path.cwd() / 'app' / 'pages'
+
+
+st.title('Chat Bot')
